6d58e9332843a1d37d1193cfc711eaeb6b612755
kuk329 <kuk329@cau.ac.kr>
2024-02-14 15:40:00 +0900

    feat: 장애발생 API 추가

--- activity_service/src/main/java/com/example/activity_service/client/UserServiceClient.java
+++ activity_service/src/main/java/com/example/activity_service/client/UserServiceClient.java
@@ -3,6 +3,7 @@ package com.example.activity_service.client;
 import com.example.activity_service.dto.request.ValidateRefreshTokenReq;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,14 @@ public interface UserServiceClient {
     @GetMapping("/users/internal/{userId}")
     void validateUserId(@PathVariable String userId);
 
+    // Error 상황 연출을 위한 api
+    @GetMapping("/users/internal/errorful/case1")
+    ResponseEntity<String> case1();
 
+    @GetMapping("/users/internal/errorful/case2")
+    ResponseEntity<String> case2();
+
+    @GetMapping("/users/internal/errorful/case3")
+    ResponseEntity<String> case3();
 
 }
